5b44101bad9bc2279bbec95d3d8f34d70237d6da
Mohammad <mo.litkouhi@gmail.com>
2020-10-04 20:24:53 +1300

    Adding src folder.

---
+++ docs/source/vuetify/tutorial_2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app class="main_container">
+    <v-app-bar
+      app
+      color="green accent-4 yellow--text"
+      dark
+    >
+     <h1 >ABI</h1>
+    </v-app-bar>
+
+    <Cube/>
+
+    <v-footer class='green accent-4 yellow--text app'>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+    import Cube from "@/components/Cube";
+
+    export default {
+      name: 'App',
+      components: {
+        Cube,
+      },
+    };
+</script>
+
+<style>
+</style>
